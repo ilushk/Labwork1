8672476354eda96047bbefc806b526abf1bf79bb
ilushk <125766905+ilushk@users.noreply.github.com>
2023-04-30 19:01:05 +0400

    Добавьте файлы проекта.

---
+++ Labaa_Vita/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QRCoder;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Labwork1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ApplicationContext db = new ApplicationContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+        }
+        // при загрузке окна
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // гарантируем, что база данных создана
+            db.Database.EnsureCreated();
+            // загружаем данные из БД
+            db.Users.Load();
+            // и устанавливаем данные в качестве контекста
+            DataContext = db.Users.Local.ToObservableCollection();
+        }
+
+        // добавление
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            UserWindow UserWindow = new UserWindow(new User());
+            if (UserWindow.ShowDialog() == true)
+            {
+                User User = UserWindow.User;
+                db.Users.Add(User);
+
+                db.SaveChanges();
+            }
+        }
+        // редактирование
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            // получаем выделенный объект
+            User? user = usersList.SelectedItem as User;
+            // если ни одного объекта не выделено, выходим
+            if (user is null) return;
+
+            UserWindow UserWindow = new UserWindow(user);
+
+            if (UserWindow.ShowDialog() == true)
+            {
+                // получаем измененный объект
+                user = db.Users.Find(UserWindow.User.Id);
+                if (user != null)
+                {
+                    user.Description = UserWindow.User.Description;
+                    user.Price = UserWindow.User.Price;
+                    user.Name = UserWindow.User.Name;
+                    user.ImgDynamic = UserWindow.User.ImgDynamic;
+                    db.SaveChanges();
+                    usersList.Items.Refresh();
+                }
+            }
+        }
+        // удаление
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            // получаем выделенный объект
+            User? user = usersList.SelectedItem as User;
+            // если ни одного объекта не выделено, выходим
+            if (user is null) return;
+            db.Users.Remove(user);
+            db.SaveChanges();
+        }
+
+
+
+
+
+        public class User
+        {
+            [Key]
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public int Price { get; set; }
+            public string? Description { get; set; }
+            [NotMapped]
+            public ImageSource? ImgDynamic { get; set; }
+        }
+        public class ApplicationContext : DbContext
+        {
+            public DbSet<User> Users { get; set; } = null!;
+            
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlite("Data Source=helloapp.db");
+            }
+        }
+
+        private void usersList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }  
+}
---
+++ Labaa_Vita/UserWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static Labwork1.MainWindow;
+using System.Drawing;
+using System.IO;
+using System.Xml.Linq;
+
+
+namespace Labwork1
+{
+    /// <summary>
+    /// Логика взаимодействия для UserWindow.xaml
+    /// </summary>
+    public partial class UserWindow : Window
+    {
+        private void GenerateQRCodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Получение ввода текста из текстовых полей
+            string text1 = Name.Text;
+            string text2 = Price.Text;
+            string text3 = Description.Text;
+
+            // Объедините вводимые тексты в одну строку
+            string text = $"{text1} {text2} {text3}";
+
+            // Сгенерируйте растровое изображение QR-кода с помощью библиотеки QRCoder
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            System.Drawing.Bitmap qrCodeBitmap = qrCode.GetGraphic(20);
+
+            // Преобразование растрового изображения QR-кода в растровое изображение WPF
+            BitmapImage qrCodeImage = new BitmapImage();
+            qrCodeImage.BeginInit();
+            System.IO.MemoryStream memoryStream = new System.IO.MemoryStream();
+            qrCodeBitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
+            memoryStream.Seek(0, System.IO.SeekOrigin.Begin);
+            qrCodeImage.StreamSource = memoryStream;
+            qrCodeImage.EndInit();
+
+            // Отображение изображения QR-кода в элементе управления изображением WPF
+            QRCodeImage.Source = qrCodeImage;
+        }
+        public User User { get; private set; }
+        public UserWindow(User user)
+        {
+            InitializeComponent();
+            User = user;
+            DataContext = User;
+            string a = User.Name + " " + User.Price + " " + User.Description;
+            QRCodeGenerator qr = new QRCodeGenerator();
+            QRCodeData data = qr.CreateQrCode(a, QRCoder.QRCodeGenerator.ECCLevel.L);
+            QRCode code = new QRCode(data);
+            Bitmap bitmap = code.GetGraphic(20);
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+                User.ImgDynamic = bitmapimage;
+            }
+        }
+
+
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
